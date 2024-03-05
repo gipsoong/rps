83e15c6925cad1d3a355ce56f0f34a6c49feb70a
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-05 21:17:08 +0800

    Create a variable to store true/false values. Create a function to check if user input is 'valid', if it is - then change the aforementioned variable to true. If not, change the variable to false and alert the user that the input is wrong. Modify the playerSelection variable to turn the prompt to uppercase for easier validation in if/else. Modify the playGame function to checkPrompt first before running the game for loop, only run the game if isPromptvalid = true, if not, then return and end the function. Start on README.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rps
+
+This aim of this project is to utilize basic Javascript (functions, loops, if else, etc.) to create a simple 'Rock Paper Scissors' game.
--- js/script.js
+++ js/script.js
@@ -1,39 +1,51 @@
 let playerScore = 0;
 let computerScore = 0;
+let isPromptValid;
 
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
     let random = Math.floor((Math.random() * choices.length));
     let computerChoice = choices[random];
 
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log("It's a tie!");
-    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        console.log('You lose this round, Paper beats Rock!');
-        return computerScore += 1;
-    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        console.log('You win this round, Rock beats Scissors!');
-        return playerScore += 1;
-    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        console.log('You win this round, Paper beats Rock!');
-        return playerScore += 1;
-    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        console.log('You lose this round, Scissors beats Paper!');
-        return computerScore += 1;
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        console.log('You win this round, Paper beats Rock!');
-        return playerScore += 1;
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        console.log('You lose this round, Rock beats Scissors!');
-        return computerScore += 1;
+function checkPrompt() {
+    if (playerSelection === 'ROCK' || playerSelection === 'PAPER' || playerSelection === 'SCISSORS') {
+        isPromptValid = true;
+        return;
+    } else if (playerSelection != 'ROCK' || playerSelection != 'PAPER' || playerSelection != 'SCISSORS') {
+        isPromptValid = false;
+        alert("Your input is not recognized, please refresh the page and enter 'Rock', 'Paper' or 'Scissors'. It does not have to be capitalized.");
+        return;
     }
 }
 
-let playerSelection = prompt('Rock, paper or scissors?');
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
+        console.log('You lose this round, Paper beats Rock!');
+        return computerScore += 1;
+    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
+        console.log('You win this round, Rock beats Scissors!');
+        return playerScore += 1;
+    } else if (playerSelection == '{PAPER}' && computerSelection == 'ROCK') {
+        console.log('You win this round, Paper beats Rock!');
+        return playerScore += 1;
+    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
+        console.log('You lose this round, Scissors beats Paper!');
+        return computerScore += 1;
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
+        console.log('You win this round, Paper beats Rock!');
+        return playerScore += 1;
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+        console.log('You lose this round, Rock beats Scissors!');
+        return computerScore += 1;
+    } 
+}
+
+let playerSelection = prompt('Rock, paper or scissors?').toUpperCase();
 
 function displayScore() {
     if (playerScore == computerScore) {
@@ -46,15 +58,21 @@ function displayScore() {
 }
 
 function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(`The current leaderboard is: Player: ${playerScore} and Computer: ${computerScore}`);
-        
-        if (i == 4) {
-            displayScore()
+    checkPrompt();
+
+    if (isPromptValid == true) {
+        for (let i = 0; i < 5; i++) {
+            let computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
+            console.log(`The current leaderboard is: Player: ${playerScore} and Computer: ${computerScore}`);
+            
+            if (i == 4) {
+                displayScore()
+            }
         }
+    } else if (isPromptValid == false) {
+        return;
     }
 }
 
-playGame();
+playGame(); 
